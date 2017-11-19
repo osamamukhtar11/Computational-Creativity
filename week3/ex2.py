56ea5ea8117412f43ef2daae14d71bddaceb503b
osamamukhtar11 <osamamukhtar11@gmail.com>
2017-11-19 21:43:34 +0200

    fixup! [e3] q2 generate_from_lhs function created

--- week3/ex2.py
+++ week3/ex2.py
@@ -38,68 +38,73 @@ def count_rhs_r(all_triples, rhs, r):
                 count = count + 1
     return count
 
-
-def generate_from_lhs(lhs, all_triples):
+def compute_cond_probability(lhs,r,rhs):
     prX = 0
     pYr = 0
-    for lhs, r, rhs in c_triples:
-        count_lh = count_lhs(all_triples, lhs)
-        count_r = count_relationships(all_triples, r)
-        count_l_r = count_lhs_r(all_triples, lhs, r)
-        count_rh = count_rhs(all_triples, rhs)
-        count_rh_r = count_rhs_r(all_triples, rhs, r)
-        print('\nCount for entity (', lhs, ') is:', count_lh)
-        print('Count for entity (', rhs, ') is:', count_rh)
-        print('Count for relationship (', r, ') is:', count_r)
-        print('Count for l-r combination is:', count_l_r)
-        print('Count for r-rh combination is:', count_rh_r)
-        prX = count_l_r / count_lh
-        print('prx =', prX)
-        pYr = count_rh_r / count_r
-        print('pYr =', pYr)
-        print()
-
-    # print('LHS:',lhs,'R:',r,'RHS:',rhs)
-    generated = []
-    for lhs1, rel1, rhs1 in all_triples:
-        if r == rel1:
-            count_lh = count_lhs(all_triples, lhs1)
-            count_r = count_relationships(all_triples, rel1)
-            count_l_r = count_lhs_r(all_triples, lhs1, rel1)
-            count_rh = count_rhs(all_triples, rhs1)
-            count_rh_r = count_rhs_r(all_triples, rhs1, rel1)
+    count_lh = count_lhs(all_triples, lhs)
+    count_r = count_relationships(all_triples, r)
+    count_l_r = count_lhs_r(all_triples, lhs, r)
+    for e in all_rhs:
+        if e != rhs:
+            #print('\nCount for left entity (', lhs, ') is:', count_lh)
+            #print('Count for relationship (', r, ') is:', count_r)
+            #count_rh = count_rhs(all_triples, e)
+            #print('- Count for right entity (', e, ') is:', count_rh)
+            count_rh_r = count_rhs_r(all_triples, e, r)
+            #print('- Count for r-rh combination is:', count_rh_r)
+            #print('Count for l-r combination is:', count_l_r)
+            #print('Count for r-rh combination is:', count_rh_r)
             prX = count_l_r / count_lh
+            #print('prx =', prX)
             pYr = count_rh_r / count_r
-            prob = prX * pYr
-            print('For given relationship, all options:', lhs1, rel1, rhs1, '(', prob, ')')
-            temp = lhs1 + ' ' + rel1 + ' ' + rhs1 + ' (' + str(prob) + ')'
-            generated.append(temp)
-            #        if(lhs == lhs1):
-            #            print('B- For given relationship,lhs, all options:', lhs1, rel1, rhs1, '(',prob,')')
+            #print('pYr =', pYr)
+            #print(prX,pYr,prX*pYr)
+    return lhs+' '+r+' '+rhs, prX, pYr, prX*pYr
 
-            # if(rhs != rhs1):
-            #    print('C- For given relationship,lhs,not rhs, all options:', lhs1, rel1, rhs1)
+def generate_from_lhs(choice,all_triples):
+    print('entity=', choice)
+    print('relations: ')
+    for j in range(len(entities)):
+        if all_triples[j][0]==choice:
+            print(' ', all_triples[j])
+            c_triples.append(all_triples[j])
+            if (not all_lhs.__contains__(all_triples[j][0])):
+                all_lhs.append(all_triples[j][0])
+            if (not all_rhs.__contains__(all_triples[j][2])):
+                all_rhs.append(all_triples[j][2])
+            if (not all_r.__contains__(all_triples[j][1])):
+                all_r.append(all_triples[j][1])
+    print()
+    print('ENTITIES:',all_lhs)
+    print('RELATIONS:',all_r)
+    print('ENTITIES',all_rhs)
+    print()
 
-    print('Generated Text:', generated)
-    return generated
+    print('All uses of relationship:')
+    for lhs,r,rhs in all_triples:
+        if r in all_r:
+            print('     ',lhs,r,rhs)
+    print()
+
+    possibilities=[]
+    for e in all_lhs:
+        for f in all_r:
+            for g in all_rhs:
+                #print('FOR the sentence:',e,f,g)
+                sent, px, py, prob = compute_cond_probability(e,f,g)
+                if(prob>0.0):
+                    #print('Possible sentence:',sent,'=>',prob,px,py)
+                    possibilities.append(str(prob)+' - '+sent)
+                    possibilities.sort(reverse=True)
+    for e in possibilities:
+        print(e)
+    return possibilities
 
 all_triples = factgen.read_triples("reverb.txt", min_attested=2)
-entities = factgen.collect_entities(all_triples, min_freq=0)
-print("Selected entities from total:", len(entities))
-c = random.choice(entities)
-check = False
+entities = factgen.collect_entities(all_triples, min_freq=1)
 c_triples=[]
-while(not check):
-    c = random.choice(entities)
-    for j in range(len(entities)):
-        if all_triples[j][0]==c:
-            check = True
-print('entity=',c)
-print('relations: ')
-for j in range(len(entities)):
-    if all_triples[j][0]==c:
-        print(' ', all_triples[j])
-        c_triples.append(all_triples[j])
-
-generate_from_lhs(c,all_triples)
-#Still need to do the ordering based on score
+all_lhs=[]
+all_rhs=[]
+all_r=[]
+QUERY_TERM= 'lunch'
+generate_from_lhs(QUERY_TERM, all_triples)
